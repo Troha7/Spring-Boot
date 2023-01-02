024066436577b072593129c964582fabcb2a9793
Trotsenko <trohae7@gmail.com>
2023-01-02 11:36:03 +0200

    Initial commit

---
+++ src/main/java/ua/hillelit/lms/javaproless34/JavaProLess34Application.java
@@ -0,0 +1,13 @@
+package ua.hillelit.lms.javaproless34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProLess34Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProLess34Application.class, args);
+    }
+
+}
---
+++ src/test/java/ua/hillelit/lms/javaproless34/JavaProLess34ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.hillelit.lms.javaproless34;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProLess34ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
